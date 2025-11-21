69a2151c72e2421612362926ae8aa6c7eec4256b
WHITEROSE <firasmoussa60@gmail.com>
2025-11-21 19:26:08 +0100

    change API endpoint to async for scale

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -8,7 +8,7 @@ from src.api.utils.op_id_generator import generate_op_id
 router = APIRouter(prefix="/api", tags=["template"])
 
 @router.get("/health", response_model=ApiResponseWrapper)
-def health_check():
+async def health_check():
     return {
         "status_code": HTTPStatus.OK,
         "operation_id": generate_op_id(),
--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ app.include_router(template_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
